aaa2086eecf8379f168c53f30e9a5cf2c92080af
Stijn Delaruelle <stijndelaruelle@gmail.com>
2017-01-08 20:56:12 +0100

    Added basic reloading & weapon switching. (+ Imported some external resources)

--- Assets/Scripts/Gameplay/Weapons/Weapon.cs
+++ Assets/Scripts/Gameplay/Weapons/Weapon.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 //TODO: Refactor this class (split it up)
 public class Weapon : MonoBehaviour
 {
+    public delegate void SwitchWeaponCallback();
+
+    [Header("Projectile options")]
+    [Space(5)]
     [SerializeField]
     private int m_NumberOfProjectiles;
 
@@ -12,8 +16,13 @@ public class Weapon : MonoBehaviour
     [SerializeField]
     private float m_Spread;
 
+    [Tooltip("Max distance that the projectile will fly.")]
     [SerializeField]
     private float m_Range;
+
+    [SerializeField]
+    private float m_ShootCooldown = 1.0f;
+    private float m_ShootCooldownTimer = 0.0f;
 
     [SerializeField]
     private Transform m_ProjectileSpawn;
@@ -21,24 +30,69 @@ public class Weapon : MonoBehaviour
     [SerializeField]
     private GameObject m_Projectile;
 
+    [Space(10)]
+    [Header("Ammo options")]
+    [Space(5)]
+    [SerializeField]
+    private float m_ReloadTime = 1.0f;
+    private float m_ReloadTimer = 0.0f;
+
+    [SerializeField]
+    private int m_MaxAmmoInClip;
+    private int m_AmmoInClip;
+
+    //Will be moved to another class so guns can share ammo types
+    [SerializeField]
+    private int m_MaxReserveAmmo;
+    private int m_ReserveAmmo;
+
+    [Space(10)]
+    [Header("Switch times")]
+    [Space(5)]
+    [SerializeField]
+    private float m_SwitchInTime = 1.0f;
+
+    [SerializeField]
+    private float m_SwitchOutTime = 1.0f;
+    private bool m_IsSwitching = false;
+
+    [Space(10)]
+    [Header("Required references")]
+    [Space(5)]
     [SerializeField]
     private Animator m_Animator;
 
     [SerializeField]
     private PlayerController m_PlayerController;
 
-    private void Update()
+    private void Awake()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Fire();
-        }
+        m_AmmoInClip = m_MaxAmmoInClip;
+        m_ReserveAmmo = m_MaxReserveAmmo;
     }
 
+    private void Update()
+    {
+        if (m_IsSwitching)
+            return;
+
+        //Cooldowns
+        HandleShootingCooldown();
+        HandleReloading();
+    }
+
+    //Shooting
     public void Fire()
     {
-        m_Animator.SetTrigger("FireTrigger");
+        //Weapon is cooling down, reloading or switching out/in
+        if (m_ShootCooldownTimer > 0.0f || m_ReloadTimer > 0.0f || m_IsSwitching == true)
+            return;
 
+        //We don't have any more ammo
+        if (AutoReload())
+            return;
+
+        //All the calculations
         Ray centerRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
 
         Vector3 forward = centerRay.direction;
@@ -70,5 +124,136 @@ public class Weapon : MonoBehaviour
                 projectile.Fire(m_PlayerController.CurrentVelocity, ray, range);
             }
         }
+
+        //Trigger the animation
+        m_Animator.SetTrigger("FireTrigger");
+
+        //Shooting consequences
+        m_ShootCooldownTimer = m_ShootCooldown;
+        m_AmmoInClip -= 1;
+
+        //Auto reload
+        AutoReload();
+    }
+
+    private void HandleShootingCooldown()
+    {
+        if (m_ShootCooldownTimer > 0.0f)
+        {
+            m_ShootCooldownTimer -= Time.deltaTime;
+
+            if (m_ShootCooldownTimer <= 0.0f)
+            {
+                m_ShootCooldownTimer = 0.0f;
+            }
+        }
+    }
+
+    //Reloading
+    public void StartReload()
+    {
+        //We don't reload (Doom/Quake style)
+        if (m_ReloadTime <= 0.0f)
+        {
+            EndReload();
+            return;
+        }
+
+        //We don't need to reload
+        if (m_AmmoInClip == m_MaxAmmoInClip)
+            return;
+
+        //If we don't have any more ammo, don't reload
+        if (m_ReserveAmmo == 0)
+            return;
+
+        //We are already reloading or switching
+        if (m_ReloadTimer > 0.0f || m_IsSwitching == true)
+            return;
+
+        //Actually start reloading
+        m_Animator.SetTrigger("ReloadTrigger");
+        m_ReloadTimer = m_ReloadTime;
+    }
+
+    private void EndReload()
+    {
+        int addedAmmo = m_MaxAmmoInClip - m_AmmoInClip;
+
+        //If we don't have enough ammo, use everything we have
+        if (m_ReserveAmmo < addedAmmo)
+            addedAmmo = m_ReserveAmmo;
+
+        m_ReserveAmmo -= addedAmmo;
+        m_AmmoInClip += addedAmmo;
+
+        m_ReloadTimer = 0.0f;
+    }
+
+    private void CancelReload()
+    {
+        m_ReloadTimer = 0.0f;
+    }
+
+    private void HandleReloading()
+    {
+        if (m_ReloadTimer > 0.0f)
+        {
+            m_ReloadTimer -= Time.deltaTime;
+
+            if (m_ReloadTimer <= 0.0f)
+            {
+                EndReload();
+            }
+        }
+    }
+
+    private bool AutoReload()
+    {
+        if (m_AmmoInClip <= 0)
+        {
+            StartReload();
+            return true;
+        }
+
+        return false;
+    }
+
+    //Switching
+    public void SwitchOut(SwitchWeaponCallback callback)
+    {
+        CancelReload();
+
+        m_Animator.SetTrigger("SwitchOutTrigger");
+        StartCoroutine(SwitchOutRoutine(callback));
+    }
+
+    public void SwitchIn(SwitchWeaponCallback callback)
+    {
+        m_Animator.SetTrigger("SwitchInTrigger");
+        StartCoroutine(SwitchInRoutine(callback));
+    }
+
+    private IEnumerator SwitchOutRoutine(SwitchWeaponCallback callback)
+    {
+        m_IsSwitching = true;
+
+        yield return new WaitForSeconds(m_SwitchOutTime);
+
+        callback();
+        m_IsSwitching = false;
+    }
+
+    private IEnumerator SwitchInRoutine(SwitchWeaponCallback callback)
+    {
+        m_IsSwitching = true;
+
+        yield return new WaitForSeconds(m_SwitchInTime);
+
+        callback();
+        m_IsSwitching = false;
+
+        //Check if this gun needs reloading
+        AutoReload();
     }
 }
---
+++ Assets/Scripts/Gameplay/Weapons/WeaponArsenal.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponArsenal : MonoBehaviour
+{
+    [SerializeField]
+    private int m_CurrentWeaponID;
+    private int m_LastWeaponID;
+
+    [SerializeField]
+    private List<Weapon> m_Weapons;
+    private bool m_IsSwitching = false;
+
+    private void Start()
+    {
+        //Disable all the weapons
+        foreach(Weapon weapon in m_Weapons)
+        {
+            weapon.gameObject.SetActive(false);
+        }
+
+        //Only enable our current weapon
+        m_Weapons[m_CurrentWeaponID].gameObject.SetActive(true);
+        m_LastWeaponID = m_CurrentWeaponID;
+    }
+
+    private void Update()
+    {
+        //Actions
+        if (Input.GetMouseButton(0))     { Fire(); }
+        if (Input.GetKeyDown(KeyCode.R)) { StartReload(); }
+
+        //Switch to the last used weapon
+        if (Input.GetKeyDown(KeyCode.A)) { SwitchWeapon(m_LastWeaponID); }
+
+        //Switch weapons with the number keys
+        if (Input.GetKeyDown(KeyCode.Keypad1)) { SwitchWeapon(0); }
+        if (Input.GetKeyDown(KeyCode.Keypad2)) { SwitchWeapon(1); }
+        if (Input.GetKeyDown(KeyCode.Keypad3)) { SwitchWeapon(2); }
+        if (Input.GetKeyDown(KeyCode.Keypad4)) { SwitchWeapon(3); }
+        if (Input.GetKeyDown(KeyCode.Keypad5)) { SwitchWeapon(4); }
+        if (Input.GetKeyDown(KeyCode.Keypad6)) { SwitchWeapon(5); }
+        if (Input.GetKeyDown(KeyCode.Keypad7)) { SwitchWeapon(6); }
+        if (Input.GetKeyDown(KeyCode.Keypad8)) { SwitchWeapon(7); }
+        if (Input.GetKeyDown(KeyCode.Keypad9)) { SwitchWeapon(8); }
+
+        //Switch weapons with the mouse wheel
+        float mouseWheel = Input.GetAxis("Mouse ScrollWheel");
+
+        if (mouseWheel != 0.0f)
+        {
+            //Previous weapon in the list
+            if (mouseWheel > 0.0f)
+            {
+                int newWeaponID = m_CurrentWeaponID - 1;
+                if (newWeaponID < 0) { newWeaponID = m_Weapons.Count - 1; }
+
+                SwitchWeapon(newWeaponID);
+            }
+
+            //Next weapon in the list
+            else
+            {
+                int newWeaponID = m_CurrentWeaponID + 1;
+                if (newWeaponID >= m_Weapons.Count) { newWeaponID = 0; }
+
+                SwitchWeapon(newWeaponID);
+            }
+        }
+    }
+
+    public void Fire()
+    {
+        m_Weapons[m_CurrentWeaponID].Fire();
+    }
+
+    public void StartReload()
+    {
+        m_Weapons[m_CurrentWeaponID].StartReload();
+    }
+
+    private void SwitchWeapon(int weaponID)
+    {
+        //Check if the weapon ID exists
+        if (weaponID < 0 || weaponID >= m_Weapons.Count)
+            return;
+
+        //Check if we are already switching
+        if (m_IsSwitching == true)
+            return;
+
+        m_LastWeaponID = m_CurrentWeaponID;
+        m_CurrentWeaponID = weaponID;
+
+        //Switching animation
+        m_Weapons[m_LastWeaponID].SwitchOut(OnWeaponSwitchedOut);
+
+        m_IsSwitching = true;
+    }
+
+    private void OnWeaponSwitchedOut()
+    {
+        m_Weapons[m_LastWeaponID].gameObject.SetActive(false);
+        m_Weapons[m_CurrentWeaponID].gameObject.SetActive(true);
+
+        //Switching animation
+        m_Weapons[m_CurrentWeaponID].SwitchIn(OnWeaponSwitchedIn);
+    }
+
+    private void OnWeaponSwitchedIn()
+    {
+        m_IsSwitching = false;
+    }
+
+    public void AddWeapon(Weapon weapon)
+    {
+        m_Weapons.Add(weapon);
+    }
+}
